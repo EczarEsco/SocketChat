5a3d971508f5a6f5450bc73148abcddda663ea8d
EczarEsco <eczar22029@gmail.com>
2023-01-29 17:02:34 -0600

    First version completed

---
+++ Client.mjs
@@ -0,0 +1,28 @@
+console.clear();
+import net from "net";
+import chalk from 'chalk';
+const port = 4000;
+const host = "192.168.0.34";
+
+const client = new net.Socket();
+client.connect(port, host);
+
+client.on("data", (data) => {
+  console.log(chalk.green(data.toString().trim()));
+}); //Mensajes de los usuarios en el servidor
+
+client.on("connect", () => {
+  process.stdin.on("data", (data) => {
+    client.write((data.toString().trim()));
+  });
+});
+
+client.on("error", (err) => {
+  if (err.errno === -4077) {
+    console.log(chalk.red("Se ha perdido comunicación con el servidor"));
+  } else {
+    console.error(err);
+  }
+
+  process.exit(0);
+});
---
+++ Server.mjs
@@ -0,0 +1,65 @@
+console.clear();
+import net from "net";
+import chalk from 'chalk';
+
+const server = net.createServer();
+const arrayClients = [];
+const port = 4000;
+const host = "192.168.0.34";
+const arrayUserNames = {};
+const Color=0
+
+//el usuario entra al servidor
+server.on("connection", (client) => {
+  if (arrayUserNames[client.remoteAddress] === undefined) {
+    arrayUserNames[client.remoteAddress] = {apodo: client.remoteAddress,R: Color+ Math.ceil(Math.random()*255),G: Color+ Math.ceil(Math.random()*255), B: Color+ Math.ceil(Math.random()*255)};
+    client.write(chalk.green("Escribe tu nombre de usuario"));
+  }
+//se hace el metodo push al array
+  arrayClients.push(client);
+
+  //se manada mensaje a todos los usuarios
+  console.log(chalk.rgb(arrayUserNames[client.remoteAddress].R,arrayUserNames[client.remoteAddress].G,arrayUserNames[client.remoteAddress].B,).underline("se ha conectado: " + arrayUserNames[client.remoteAddress].apodo));
+  client.on("data", (data) => {
+    if (arrayUserNames[client.remoteAddress].apodo === client.remoteAddress) {
+      arrayUserNames[client.remoteAddress].apodo = data.toString().trim();
+      client.write(chalk.rgb(arrayUserNames[client.remoteAddress].R,arrayUserNames[client.remoteAddress].G,arrayUserNames[client.remoteAddress].B,).underline("tu nickName ahora es: " + arrayUserNames[client.remoteAddress].apodo));
+      return;
+    }
+    for (const key in arrayClients){
+      if(arrayClients[key].remoteAddress ===client.remoteAddress) continue
+      arrayClients[key].write(chalk.rgb(arrayUserNames[client.remoteAddress].R,arrayUserNames[client.remoteAddress].G,arrayUserNames[client.remoteAddress].B).underline(arrayUserNames[client.remoteAddress].apodo) + ":" + data);
+    }
+
+    console.log(chalk.rgb(arrayUserNames[client.remoteAddress].R,arrayUserNames[client.remoteAddress].G,arrayUserNames[client.remoteAddress].B,).underline(arrayUserNames[client.remoteAddress].apodo) + ":" + data);
+  });
+
+  //El usuario abandona el servidor
+  client.on("close", () => {
+    arrayClients.map((usuario) => {
+      usuario.write(chalk.rgb(arrayUserNames[client.remoteAddress].R,arrayUserNames[client.remoteAddress].G,arrayUserNames[client.remoteAddress].B,).underline(arrayUserNames[client.remoteAddress].apodo) + chalk.red(" ha salido del servidor"));
+    });
+  });
+//Cuando ocurre un error
+  client.on("error", (err) => {
+    if (err.errno === -4077) {
+      arrayClients.map((usuario) => {
+        usuario.write(chalk.rgb(arrayUserNames[client.remoteAddress].R,arrayUserNames[client.remoteAddress].G,arrayUserNames[client.remoteAddress].B,).underline(arrayUserNames[client.remoteAddress].apodo) + chalk.red(" ha salido del servidor"));
+      });
+
+      console.log(chalk.rgb(arrayUserNames[client.remoteAddress].R,arrayUserNames[client.remoteAddress].G,arrayUserNames[client.remoteAddress].B,).underline(arrayUserNames[client.remoteAddress].apodo) + chalk.red(" ha salido del servidor"));
+    } else {
+      console.error(err);
+    }
+  });
+});
+server.on("error", (err) => {
+  console.log(chalk.red(err));
+});
+
+//Notificacion al servidor cuando un usuario entra
+server.listen(port, host, () => {
+  console.log(chalk.green("Servidor a la escucha"));
+});
+
+
